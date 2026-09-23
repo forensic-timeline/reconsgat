2fa091a57bf6ce18f3259cda2290c648a49bcc95
yasirutomo <yasirutomo@gmail.com>
2026-09-23 07:46:52 +0700

    log decoder

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 
 from process.normalization import Normalization
+from process.log_decoder import LogDecoder
 
 
 if __name__ == "__main__":
@@ -16,5 +17,10 @@ if __name__ == "__main__":
     print("=== EVENT RECONSTRUCTION PROCESS ===")
     print("="*80)
 
+    print("")
     print("=== Step 1: Normalization ===")
     Normalization(dataset).run()
+
+    print("")
+    print("=== Step 2: Log Decoder ===")
+    LogDecoder(dataset).run()
