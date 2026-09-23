127825f645c88b5e2b8b3e8d841fc9d855800aa6
yasirutomo <yasirutomo@gmail.com>
2026-09-23 07:06:16 +0700

    process dir to package

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import sys
 
-# Supaya bisa "from normalization import Normalization" tanpa prefix "process."
-sys.path.insert(0, "process")
-
-from normalization import Normalization
+from process.normalization import Normalization
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: python main.py <dataset_name>")
-        print("Example: python main.py sample")
+        print("Example: python main.py 1sample")
         sys.exit(1)
 
     dataset = sys.argv[1]
